df95a87a02a9b7cf8fac9ec30843906c080bac66
Norly <canariasnorly@gmail.com>
2017-12-10 13:05:12 +0800

    Setups application pack; Also adds main component for store page and a loader component

---
+++ app/javascript/store_page/components/main.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+class Main extends Component{
+  render(){
+    return(
+      <div>
+        Welcome to Happy Shop!
+      </div>
+    )
+  }
+
+}
+
+export default Main
---
+++ app/javascript/store_page/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Main from "./components/main"
+
+document.addEventListener("turbolinks:load", function(){
+  const root = document.getElementById("store-page")
+  if(root){
+    ReactDOM.render(<Main />, root)
+  }
+})

f85bc3f201ca5248c9266412049c57670a761250
Norly <canariasnorly@gmail.com>
2017-12-10 13:42:09 +0800

    Enables fetching in js and fixes alignment;

--- app/javascript/store_page/components/main.js
+++ app/javascript/store_page/components/main.js
@@ -1,6 +1,44 @@
 import React, {Component} from "react"
 
+class ProductsFetcher{
+  constructor(){
+    this.endpoint = "/api/v1/products"
+  }
+
+  perform(fetchOptions, successCallback){
+    $.ajax({
+      url:        this.endpoint,
+      data:       fetchOptions,
+      beforeSend: (request) => {
+        request.setRequestHeader("Content-Type", "application/vnd.api+json")
+        request.setRequestHeader("Accept",       "application/vnd.api+json")
+      },
+      success: successCallback,
+      error:   (response) => {
+        alert("Woops, an error occured.")
+      }
+    })
+  }
+}
+
 class Main extends Component{
+  constructor(props){
+    super(props)
+    this.state = {products: [], fetchOptions: {}}
+  }
+
+  componentDidMount(){
+    this.fetchRecords()
+  }
+
+  fetchRecords(){
+    const fetcher = new ProductsFetcher
+    fetcher.perform(this.state.fetchOptions, ((response) =>{
+      console.log("Back in main")
+      this.setState({products: response.data, meta: response.meta})
+    }).bind(this))
+  }
+
   render(){
     return(
       <div>
@@ -8,7 +46,6 @@ class Main extends Component{
       </div>
     )
   }
-
 }
 
 export default Main
--- app/javascript/store_page/index.js
+++ app/javascript/store_page/index.js
@@ -1,6 +1,6 @@
-import React from "react"
+import React    from "react"
 import ReactDOM from "react-dom"
-import Main from "./components/main"
+import Main     from "./components/main"
 
 document.addEventListener("turbolinks:load", function(){
   const root = document.getElementById("store-page")

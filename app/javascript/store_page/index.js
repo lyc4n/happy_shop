8bf2e2bcfff4a520637dc9797e450334c83b15cb
Norly <canariasnorly@gmail.com>
2017-12-12 12:00:19 +0800

    Removes turbolink

--- app/javascript/store_page/index.js
+++ app/javascript/store_page/index.js
@@ -3,18 +3,10 @@ import ReactDOM      from "react-dom"
 import Main          from "./components/main"
 import ProductDetail from "./components/product_detail"
 
-document.addEventListener("turbolinks:load", function(){
+$(function(){
   const root = document.getElementById("store-page")
   if(root){
     const store  = JSON.parse(root.dataset.store)
     ReactDOM.render(<Main store={store}/>, root)
-  }
-})
-
-document.addEventListener("turbolinks:load", function(){
-  const root = document.getElementById("book-detail")
-  if(root){
-    const productDetail = JSON.parse(root.dataset.detail)
-    ReactDOM.render(<ProductDetail {...(productDetail.data.attributes)} />, root)
   }
 })

665a612a7a8842d5b7315b126231a381a054a6e0
Norly <canariasnorly@gmail.com>
2017-12-12 12:11:45 +0800

    Removes /books route and books controller
    - This is because only the root page will be rendering html and in order
      to utilize /api/v1/books/:id endpoint

--- app/controllers/products_controller.rb
+++
@@ -1,7 +0,0 @@
-class ProductsController < ApplicationController
-  def show
-    product  = Product.find(params[:id])
-    renderer = JSONAPI::Serializable::Renderer.new
-    @product = renderer.render(product, {class: {Product: SerializableProduct}}).to_json
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "stores#show"
-  resources :products, only: :show
   namespace :api do
     namespace :v1 do
       resources :products, constraints: JsonApiConstraint.new
